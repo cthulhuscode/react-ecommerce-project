04e6bd41277f8e7e526cb4ecaf52031943cf8797
Enrique Ruvalcaba <jordison352@gmail.com>
2023-02-10 13:21:53 -0600

    new images

--- src/constants/images.js
+++ src/constants/images.js
@@ -3,6 +3,9 @@ import lupa from "../assets/lupa.png";
 import cart from "../assets/cart.png";
 import account from "../assets/account.png";
 import line from "../assets/line.png";
+import menu from "../assets/menu.svg";
+import close from "../assets/close.svg";
+import logout from "../assets/logout.svg";
 
 export const images = {
 	logo,
@@ -10,4 +13,7 @@ export const images = {
 	line,
 	cart,
 	account,
+	menu,
+	close,
+	logout,
 };
