717379f8d1f874cfc42e2dda4ce6efa1e716c92a
Héctor Alejandro <hector.alejandro2221@gmail.com>
2023-02-10 20:41:46 -0600

    bug category

--- src/layout/Categories/Categories.jsx
+++ src/layout/Categories/Categories.jsx
@@ -1,9 +1,8 @@
-import React, {useEffect , useState } from "react";
+import {useEffect , useState } from "react";
 import "./Categories.scss";
 import axios  from "axios";
 
 export const Categories = () => {
-  const [data,setData] = useState([]);
   const [categories, setCategories] = useState([]);
 
   useEffect(() => {
@@ -11,7 +10,6 @@ export const Categories = () => {
       try {
           const res = await axios('https://fakestoreapi.com/products')
           const response = await res.data;
-          setData(response)
           setCategories(removeDuplicates(response.map(item => item.category)));
       } catch (error) {
           console.log(error);
@@ -30,8 +28,8 @@ export const Categories = () => {
       <div className="category-flex ">
         {
           categories.map((category) => 
-              <a className="category-item" href="#"> {/*Falta darle enlaces a las categorias */}
-                <h4 className="category_tittle-item" key={category}>{category}</h4>
+              <a className="category-item" href="#"  key={category}> {/*Falta darle enlaces a las categorias */}
+                <h4 className="category_tittle-item">{category}</h4>
               </a>
           )
         }
