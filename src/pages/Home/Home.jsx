1249495806f0fcd0a49218ccbce7ec1b58cbb968
Hector Alejandro <hector.alejandro2221@gmail.com>
2023-02-10 20:14:38 -0600

    pages category add

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,11 @@
 import { Navbar } from "../../layout/Navbar/Navbar"
+import { Categories } from "../../layout/Categories/Categories"
 
 export const Home = () => {
   return (
     <div className="home">
       <Navbar />
+      <Categories/>
     </div>
   )
 }
