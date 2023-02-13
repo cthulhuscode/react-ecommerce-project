0745ee2f0c84130e550a0ea1d98a9d567ae4f4cd
Enrique Ruvalcaba <jordison352@gmail.com>
2023-02-13 13:46:11 -0600

    changing <a> for Link and NavLink

--- src/layout/Navbar/Navbar.jsx
+++ src/layout/Navbar/Navbar.jsx
@@ -2,10 +2,12 @@ import { images } from "../../constants/images";
 import {motion} from "framer-motion";
 import "./Navbar.scss";
 import { useState } from "react";
+import { Link, NavLink } from "react-router-dom";
 
 
 export const Navbar = () => {
   const [toggle, setToggle] = useState(false);
+  const isAuth = false;
 
   const handleMenuClick = (e) => {
     // e.stopPropagation();
@@ -14,11 +16,21 @@ export const Navbar = () => {
 
   return (
     <nav className="navbar">
-      <img className="navbar__logo" src={images.logo} alt="Shoppe" />
 
+      <Link to="/">
+        <img className="navbar__logo" src={images.logo} alt="Shoppe" />
+      </Link>
+
+      {/* Desktop menu */}
       <ul className="navbar-list">
         <li className="navbar-list__item">
-          <a className="navbar-list__anchor" href="#">Shop</a>
+          <NavLink 
+            to="/products"
+            className={({ isActive }) => isActive 
+              ? "navbar-list__anchor navbar-list__anchor--active" 
+              : "navbar-list__anchor"
+            }>Shop
+          </NavLink>
         </li>
         <li className="navbar-list__item">
           <img className="navbar-list__icon" src={images.line} alt="line" />          
@@ -46,8 +58,8 @@ export const Navbar = () => {
         </li>
       </ul>
 
+      {/* Mobile menu */}
       <div className="navbar-menu">
-
         <motion.img 
           whileHover={{scale: 1.15}}
           whileTap={{scale: 0.8}}
@@ -81,17 +93,39 @@ export const Navbar = () => {
             </motion.span>
 
             <ul className="navbar-menu__list">
-              <li className="navbar-menu__item">Home</li>
-              <li className="navbar-menu__item">Shop</li>
-              <li className="navbar-menu__item">Cart</li>
-              <hr className="navbar-menu__separator"/>
+
               <li className="navbar-menu__item">
-                <img src={images.account} alt="" />
-                <span>My account</span>
+                <Link className="navbar-menu__item-link" to="/">Home</Link>
               </li>
+
               <li className="navbar-menu__item">
-                <img src={images.logout} alt="logout" />
-                <span>Logout</span>
+                <Link className="navbar-menu__item-link" to="/products">Shop</Link>
+              </li>
+
+              <li className="navbar-menu__item">
+                <Link className="navbar-menu__item-link" to="/checkout">Cart</Link>
+              </li>
+
+              <hr className="navbar-menu__separator"/>
+
+              <li className="navbar-menu__item">
+                <Link className="navbar-menu__item-link" to="/products">                  
+                  <img className="navbar-menu__item-icon" src={images.account} alt="" />
+                  <span className="navbar-menu__item-text">My account</span>
+                </Link>
+              </li>
+
+              <li className="navbar-menu__item">
+                {isAuth 
+                  ? <div className="navbar-menu__item-link">                      
+                      <img className="navbar-menu__item-icon" src={images.logout} alt="logout" />
+                      <span className="navbar-menu__item-text">Logout</span>
+                    </div> 
+                  : <Link className="navbar-menu__item-link" to="/login">                  
+                      <img className="navbar-menu__item-icon" src={images.login} alt="logout" style={{width: "24px", height: "24px"}}/>
+                      <span className="navbar-menu__item-text">Login</span>
+                    </Link>
+                }
               </li>
             </ul>
           </motion.div>
