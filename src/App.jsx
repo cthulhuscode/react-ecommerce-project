a18707fb1bfb27631c78a30d33d322b35f145b22
Enrique Ruvalcaba <jordison352@gmail.com>
2023-02-13 17:01:37 -0600

    adding routes and improving design

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,22 @@
-import { Home } from "./pages/Home/Home";
-import "./App.scss"
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-
+import { HomePage, ProductsPage, ProductPage } from "./pages";
 import { Navbar } from "./layout/Navbar/Navbar";
-import { ProductCard } from "./components/ProductCard/ProductCard"
+import "./App.scss"
 
 const App = () => {
 	return <div className="app">
 		<BrowserRouter>
-		{/* Shows in all pages */}
-		<Navbar />		
-		<Routes>
-			<Route path="/" element={<Home />} />			
-			<Route path="/products" element={<ProductCard />} />			
+			{/* Shows in all pages */}
+			<Navbar />	
+				
+			<Routes>
+				<Route path="/" element={<HomePage />} />			
+				<Route path="/products" element={<ProductsPage />} />			
+				<Route path="/products/:id" element={<ProductPage />} />	
 
-			{/* This must always be at the end */}
-			<Route path="*" element={<h1>Not found</h1>}/>
-		</Routes>
+				{/* This must always be at the end */}
+				<Route path="*" element={<h1>Not found</h1>}/>
+			</Routes>
 		</BrowserRouter>
 	</div>;
 };
