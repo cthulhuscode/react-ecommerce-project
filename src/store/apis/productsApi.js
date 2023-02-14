9a30d8aecf4e4808e989cd0c7a2fabfa8af24903
Enrique Ruvalcaba <jordison352@gmail.com>
2023-02-14 10:51:48 -0600

    Changing API URL

--- src/store/apis/productsApi.js
+++ src/store/apis/productsApi.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const productsApi = createApi({
 	reducerPath: "products",
 	baseQuery: fetchBaseQuery({
-		baseUrl: "http://localhost:5000/api",
+		baseUrl: import.meta.env.VITE_API_URL,
 	}),
 	endpoints: (builder) => ({
 		getProducts: builder.query({
