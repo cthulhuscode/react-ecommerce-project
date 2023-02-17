be3ad26137a077231d59476900f5ca8b4c646e00
Hector Alejandro <hector.alejandro2221@gmail.com>
2023-02-17 17:03:28 -0600

    footer agregado

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,33 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { HomePage, ProductsPage, ProductPage } from "./pages";
 import { Navbar } from "./layout/Navbar/Navbar";
-import "./App.scss"
+import { Footer } from "./layout/Footer/Footer";
+import "./App.scss";
 import { Cart } from "./components";
 import { useSelector } from "react-redux";
 
 const App = () => {
+	const showCart = useSelector((state) => state.cart.show);
 
-	const showCart = useSelector(state => state.cart.show);
+	return (
+		<div className="app">
+			<BrowserRouter>
+				{/* Shows in all pages */}
+				<Navbar />
+				{showCart && <Cart />}
 
-	return <div className="app">
-		<BrowserRouter>
-			{/* Shows in all pages */}
-			<Navbar />	
-			{showCart && <Cart />}
-				
-			<Routes>
-				<Route path="/" element={<HomePage />} />			
-				<Route path="/products" element={<ProductsPage />} />			
-				<Route path="/products/:id" element={<ProductPage />} />	
+				<Routes>
+					<Route path="/" element={<HomePage />} />
+					<Route path="/products" element={<ProductsPage />} />
+					<Route path="/products/:id" element={<ProductPage />} />
 
-				{/* This must always be at the end */}
-				<Route path="*" element={<h1>Not found</h1>}/>
-			</Routes>
-		</BrowserRouter>
-	</div>;
+					{/* This must always be at the end */}
+					<Route path="*" element={<h1>Not found</h1>} />
+				</Routes>
+				<Footer />
+			</BrowserRouter>
+		</div>
+	);
 };
 
 export default App;
