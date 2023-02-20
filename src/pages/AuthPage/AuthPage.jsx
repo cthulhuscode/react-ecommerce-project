e6415e34ecc45d6f04f30be93af60fc3650920ae
Enrique Ruvalcaba <jordison352@gmail.com>
2023-02-20 08:07:35 -0600

    Enhancing Auth page

--- src/pages/AuthPage/AuthPage.jsx
+++ src/pages/AuthPage/AuthPage.jsx
@@ -1,47 +1,63 @@
-import { useParams, Link } from 'react-router-dom'
-import './AuthPage.scss'
+import { useState } from "react";
+import { Button } from "../../ui";
+import "./AuthPage.scss";
+
+const Operations = {
+    signIn: "Sign In",
+    register: "Register"
+}
 
 export const AuthPage = () => {
-    const { auth } = useParams()
+    const [operation, setOperation] = useState(Operations.signIn);
+
+    const handleTabClick = (e) => {
+        const op = e.target.name;
+        setOperation(op);
+    }
 
     return (
-        <div className="formulario">
-            <h1>My Account</h1>
-            <div className="logreg">
-                <Link to="/auth/register">
-                    <label className={`label reg__label ${auth === 'register' ? 'label__selected' : ''}`}> Register</label>
-                </Link>
-                <Link to="/auth/login">
-                    <label className={`label reg__label ${auth === 'login' ? 'label__selected' : ''}`}> Sign In</label>
-                </Link>
+        <div className="auth">
+            <h1 className="auth__title">My Account</h1>
 
-                {/* <label className={`label reg__label ${auth==='register'?'label__selected':''}`}> Register </label> */}
-                {/* <label className={`label reg__label ${auth==='login'?'label__selected':''}`}> Sign In </label> */}
+            <div className="auth__tabs">
+                <button
+                    className={`auth__tab 
+                        ${operation === Operations.signIn && 'auth__tab--active'}                    
+                    `}
+                    onClick={handleTabClick}
+                    name={Operations.signIn}
+                >
+                    {Operations.signIn}
+                </button>
+                <button
+                    className={`auth__tab 
+                        ${operation === Operations.register && 'auth__tab--active'}
+                    `}
+                    onClick={handleTabClick}
+                    name={Operations.register}
+                >
+                    {Operations.register}
+                </button>
             </div>
-            <form method="post">
-                <div className={`username__not ${auth === 'register' ? 'username' : ''}`}>
-                    <input type="username" required />
-                    <label>Email</label>
+
+            <form className="auth__form" action="">
+                <input
+                    className="auth__form-control auth__form-text"
+                    style={{ display: `${operation !== Operations.register ? "none" : ""}` }}
+                    type="text"
+                    placeholder="Name"
+                    required
+                />
+                <input className="auth__form-control auth__form-text" type="text" required placeholder="Email" />
+                <input className="auth__form-control auth__form-text" type="password" required placeholder="Password" />
+                <div className="auth__form-control auth__form-ckb-div">
+                    <input className="auth__form-checkbox" type="checkbox" name="remember" id="remember" />
+                    <label className="auth__form-label" htmlFor="remember">Remember me</label>
                 </div>
-                <div className="username register__selected">
-                    <input type="username register__selected" required />
-                    <label>Username</label>
-                </div>
-                <div className="username">
-                    <input type="password" required />
-                    <label>Password</label>
-                </div>
-                <div className="recordar">
-                    <p>
-                        <label><input className="mycheck" type="checkbox" /> Recordarme</label>
-                    </p>
-                </div>
-                {/* <input className="inputvalue" type="submit" value="SIGN IN" /> */}
-                <button className='buttonvalue' >SIGN IN</button>
-                <div className="registrarse">
-                    Have you forgotten your password?<br />
-                </div>
+
+                <Button classes={"auth__form-control auth__form-btn"} variant={"black"}>Sign In</Button>
             </form>
+            <p className="auth__forget">Have you forgotten your password?</p>
         </div>
-    )
-}
+    );
+};
